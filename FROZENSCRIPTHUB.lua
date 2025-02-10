97c4a33f51091dfe552b0eddb01a458b6b5b86ef
geeraise <anthonyant878@gmail.com>
2025-02-10 22:00:44 +0700

    Add files via upload

---
+++ FROZENSCRIPTHUB.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 98860129550302 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/geeraise/A-bit-balanced-soul-ops-but-kinda-fre-C-insanity-Script-Hub/refs/heads/main/ABBSOBKF.txt"))() -- abbsobutkindafree
+elseif game.PlaceId == 118477791488473 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/geeraise/A-bit-balanced-soul-ops-but-kinda-fre-C-insanity-Script-Hub/refs/heads/main/HalloweenEvent.txt"))() -- halloween event (abbsobutkindafree)
+elseif game.PlaceId == 77064819698951 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/geeraise/A-bit-balanced-soul-ops-but-kinda-fre-C-insanity-Script-Hub/refs/heads/main/DRAGONGAMEREVENT.txt"))() -- dragongamer event (abbsobutkindafree)
+end
